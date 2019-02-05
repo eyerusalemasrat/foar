8fceb74ceb3f3a154e3a89e345625e681957a240
edencheru <45384298+edencheru@users.noreply.github.com>
2019-02-05 05:05:30 -0800

    Add files via upload

---
+++ src/test/java/com/coderz/foar/FOARTests.java
@@ -0,0 +1,16 @@
+package com.coderz.foar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FOARTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
