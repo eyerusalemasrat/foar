cbdc58e8bc7982da3b7f98afd7cf15be6b33a7a3
eyerusalemasrat <45384135+eyerusalemasrat@users.noreply.github.com>
2019-02-05 04:38:08 -0800

    Add files via upload

---
+++ src/main/java/com/coderz/foar/repositories/DeliveryRepository.java
@@ -0,0 +1,9 @@
+package com.coderz.foar.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.coderz.foar.domains.Delivery;
+
+public interface DeliveryRepository extends CrudRepository<Delivery,Long>{
+
+}
