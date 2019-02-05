7bf65d482966a28c9f96ac362b0ba39cd3ab9a1f
eyerusalemasrat <45384135+eyerusalemasrat@users.noreply.github.com>
2019-02-05 04:36:09 -0800

    Add files via upload

---
+++ src/main/java/com/coderz/foar/security/RecipieRepository.java
@@ -0,0 +1,8 @@
+package com.coderz.foar.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.coderz.foar.domains.Recipie;
+
+public interface RecipieRepository extends CrudRepository<Recipie,Long>{
+
+}
---
+++ src/main/java/com/coderz/foar/security/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.coderz.foar.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.coderz.foar.domains.Restaurant;
+
+
+public interface RestaurantRepository extends CrudRepository<Restaurant, Long>{
+
+}
