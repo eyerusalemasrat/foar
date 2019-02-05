5fd3d5f653c31fa600ae21208af83cd05969b9c4
G2mcab <34392821+G2mcab@users.noreply.github.com>
2019-02-05 04:51:34 -0800

    Delete UserRepository.java

--- src/main/java/com/coderz/foar/security/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coderz.foar.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.coderz.foar.security.User;
-
-public interface UserRepository extends CrudRepository<User, Long>{
-	public User findByUsernameAndPassword(String Username, String Password);
-}
