4cf22171bc67c92a2c4a0b36b72d4f439d27bece
G2mcab <34392821+G2mcab@users.noreply.github.com>
2019-02-24 14:52:43 -0800

    Add files via upload

--- src/main/java/com/coderz/foar/FOAR.java
+++ src/main/java/com/coderz/foar/FOAR.java
@@ -2,10 +2,16 @@ package com.coderz.foar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import de.codecentric.boot.admin.client.registration.Application;
 
 @SpringBootApplication
 public class FOAR {
-
+	
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources(Application.class); 
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FOAR.class, args);
 	}
